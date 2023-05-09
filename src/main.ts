de2a770d4daaa56b1b0c8845d725826ea16b540f
petereon <pvyboch1@gmail.com>
2023-05-09 19:52:49 +0200

    collision fun

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
+import typescriptLogo from './ts.png'
+import javascriptLogo from './js.png'
 import wall from './wall.jpg'
 
 
@@ -17,16 +18,19 @@ const app = new PhysicsApp({
 document.querySelector<HTMLDivElement>('#app')?.appendChild(app.view as HTMLCanvasElement);
 
 const loadTextures = async () => {
-  return { typescriptTexture: await Assets.load(typescriptLogo), wallTexture: await Assets.load(wall) }
+  return { typescriptTexture: await Assets.load(typescriptLogo), wallTexture: await Assets.load(wall), javascriptTexture: await Assets.load(javascriptLogo) }
 }
 
-loadTextures().then(({ typescriptTexture, wallTexture }) => {
+loadTextures().then(({ typescriptTexture, wallTexture, javascriptTexture }) => {
   const logo = new RectangleBody({ x: 300, y: 100, height: 50, width: 50, properties: { restitution: 0.9 }, texture: typescriptTexture });
   const ground = new RectangleBody({ x: window.innerWidth / 2, y: window.innerHeight - 100, width: window.innerWidth, height: 200, properties: { isStatic: true }, texture: wallTexture });
   const leftWall = new RectangleBody({ x: 0, y: window.innerHeight / 2, width: 20, height: window.innerHeight, properties: { isStatic: true }, texture: wallTexture });
   const rightWall = new RectangleBody({ x: window.innerWidth, y: window.innerHeight / 2, width: 20, height: window.innerHeight, properties: { isStatic: true }, texture: wallTexture });
   const topWall = new RectangleBody({ x: window.innerWidth / 2, y: 0, width: window.innerWidth, height: 20, properties: { isStatic: true }, texture: wallTexture });
 
+  const jsStack = [1,2,3,4,5,6,7].map(i => {
+    return new RectangleBody({ x: 1000, y: window.innerHeight-200 - (i*51), height: 50, width: 50, properties: { restitution: 0.9 }, texture: javascriptTexture })
+  });
 
   const keys: { [key: string]: boolean } = {};
 
@@ -59,7 +63,7 @@ loadTextures().then(({ typescriptTexture, wallTexture }) => {
     }
   }
 
-  app.addBodies(ground, logo, leftWall, rightWall, topWall);
+  app.addBodies(ground, logo, leftWall, rightWall, topWall, ...jsStack);
 
   app.ticker.add(() => {
     app.update();
